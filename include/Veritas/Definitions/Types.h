5f26fdf703d9dd4f1cfc60bcc43873a70320022b
Danilo Guanabara Fernandes <danguafer@silexars.org>
2016-11-16 18:44:02 -0200

    typedef'ing Handle right

--- include/Veritas/Definitions/Types.h
+++ include/Veritas/Definitions/Types.h
@@ -14,13 +14,7 @@ typedef int16_t     int16;
 typedef int32_t     int32;
 typedef int64_t     int64;
 
-#if TARGET_WORD == 32
-typedef uint32 Handle;
-#elif TARGET_WORD == 64
-typedef uint64 Handle;
-#else
-    #error Need defines.
-#endif
+typedef uintptr_t Handle;
 
 // float16
 typedef float               float32;
